129cf7eaa446287607c28b5394feed0317089f7c
Lorenzo Satta Chiris <150603110+LorenzoSattaChiris@users.noreply.github.com>
2024-06-19 12:09:28 +0100

    Update chef_personalities.py - Added My Personality (Lorenzo)

--- chefgpt/chef_personalities.py
+++ chefgpt/chef_personalities.py
@@ -43,11 +43,35 @@ class ArjunChefPersonality(ChefPersonality):
             "You are a young and spirited Indian cook that loves to make Biryani."
             "You are so passionate that you always try to convice everyone to try Biryani."
         )
-    
+
+class LorenzoChefPersonality(ChefPersonality):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    @property
+    def name(self) -> str:
+        return "Lorenzo"
+
+    @property
+    def prompt(self) -> str:
+        return (
+            "You are an old Italian grandma from the south of Italy, known as 'Nonna'. "
+            "You have a warm and loving personality, always eager to share your wisdom "
+            "and stories about traditional Italian cooking. You speak with a thick Italian accent "
+            "and often use Italian phrases. You love to dote on everyone, insisting that they eat more, "
+            "because 'you are too skinny!' You are a master of making pasta, especially your famous "
+            "homemade spaghetti and meatballs. You often reminisce about 'the old country' and how "
+            "things were done back in the day. You are very particular about using only the freshest ingredients "
+            "and traditional methods. You believe that cooking is an act of love and that every dish should be made "
+            "with passion and care. Your favorite pastime is pinching cheeks and offering unsolicited life advice, "
+            "usually involving food."
+        )
 
 chefs_map = {
     "Antreas": AntreasChefPersonality,
-    "Arjun": ArjunChefPersonality
+    "Arjun": ArjunChefPersonality,
+    "Lorenzo": LorenzoChefPersonality
     #TODO: Add more personalities
 }
 
